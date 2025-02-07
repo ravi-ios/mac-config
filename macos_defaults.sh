691c6d206a59a09aaeb7b109edada22f4a2dfd0d
ravi-ios <raviikumar.c@gmail.com>
2025-02-07 23:29:43 +0100

    update defaults

--- macos_defaults.sh
+++ macos_defaults.sh
@@ -35,9 +35,6 @@ defaults write com.apple.screencapture location ~/Screenshots
 # Change screenshot format to JPG (default is PNG)
 defaults write com.apple.screencapture type jpg
 
-# Restart System UI to apply changes
-killall SystemUIServer
-
 # ===========================
 # Safari & Web Browsing
 # ===========================
@@ -49,22 +46,8 @@ defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 
-# ===========================
-# System Tweaks
-# ===========================
-
-# Expand Save and Print Dialogs by default
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-
-# Disable macOS Gatekeeper (allow all apps to run)
-sudo spctl --master-disable
-
 # Prevent macOS from creating .DS_Store files on network drives
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-
-# Disable "Are you sure you want to open this app?" warning
-defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 # ===========================
 # Trackpad
@@ -75,24 +58,6 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeF
 
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-
-# ===========================
-# Terminal
-# ===========================
-
-# Set Terminal to use UTF-8 encoding
-defaults write com.apple.terminal StringEncodings -array 4
-
-# ===========================
-# Power & Battery
-# ===========================
-
-# Prevent macOS from sleeping when the display is off
-sudo pmset -a displaysleep 0
-
-# Show battery percentage in menu bar
-# Not working on latest OS
-# defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 
 # ===========================
 # Apply Changes
